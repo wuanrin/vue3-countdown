47428728445572633c32feebd0a03d84d376749b
wuanrin <master@yusong.me>
2022-07-13 21:53:01 +0800

    TypeScript Support

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,35 @@
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+const banner = `/**
+ * ${pkg.name} v${pkg.version}
+ * (c) 2021 ${pkg.author}
+ * Released under the ${pkg.license} license.
+ */`
 
 export default {
   input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    exports: 'auto',
-    format: 'cjs',
-    name: 'Vue3Countdown'
-  },
-  external: [
-    'vue'
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      exports: 'auto',
+      name: 'Vue3Countdown',
+      banner,
+      globals: {
+        vue: 'Vue'
+      }
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      banner
+    }
   ],
+  external: Object.keys(pkg.peerDependencies),
   plugins: [
-    typescript()
+    typescript({
+      typescript: require('typescript')
+    })
   ]
 }
